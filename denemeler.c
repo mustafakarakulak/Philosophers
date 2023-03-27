583e9773c34e923c21e337c62d467b202d0b3b88
Mustafa Karakulak <mustafa.monster46@gmail.com>
2023-03-27 17:47:45 +0300

    not finished

--- denemeler.c
+++ denemeler.c
@@ -1,19 +1,38 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   denemeler.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mkarakul <mkarakul@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/19 17:12:10 by mkarakul          #+#    #+#             */
-/*   Updated: 2023/03/19 17:12:17 by mkarakul         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
 
-int	main()
+/*thread function definition*/
+void	*threadFunction(void *args)
 {
-	
+	while (1)
+	{
+		sleep(1);
+		printf("I am threadFunction.\n");
+	}
+}
+int main()
+{
+	/*creating thread id*/
+	pthread_t *id;
+	int ret;
+
+	/*creating thread*/
+	ret = pthread_create(id, NULL, threadFunction, NULL);
+	if (ret == 0)
+	{
+		printf("Thread created successfully.\n");
+	}
+	else
+	{
+		printf("Thread not created.\n");
+		return 0; /*return from main*/
+	}
+	while (1)
+	{
+		sleep(1);
+		printf("I am main function.\n");
+	}
+
+	return 0;
 }
